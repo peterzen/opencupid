e83c5842a0b1f5d054ee004cb9a4fed10f1be2ff
Peter Banik <peter@froggle.org>
2025-06-02 19:46:22 +0200

    Gender Icon component

---
+++ apps/frontend/src/components/profiles/GenderSymbol.vue
@@ -0,0 +1,30 @@
+
+<script lang="ts" setup>
+import { GenderType } from '@zod/generated';
+import { genderIcons } from './genderIcons';
+
+
+const props = defineProps<{
+  gender: GenderType
+}>()
+
+const IconComponent = genderIcons[props.gender]
+
+
+
+
+</script>
+
+<template>
+
+  <component :is="IconComponent" class="gender-icon" />
+
+</template>
+
+<style scoped>
+.gender-icon {
+  width: 2rem;
+  height: 2rem;
+  fill: currentColor;
+}
+</style>
---
+++ apps/frontend/src/components/profiles/genderIcons.ts
@@ -0,0 +1,47 @@
+
+
+import MaleIcon  from '@/assets/icons/gender/male.svg'
+import FemaleIcon from '@/assets/icons/gender/female.svg'
+import AgenderIcon from '@/assets/icons/gender/genderless.svg'
+import AndrogynousIcon from '@/assets/icons/gender/other-gender.svg'
+import BigenderIcon from '@/assets/icons/gender/other-gender.svg'
+import CisManIcon from '@/assets/icons/gender/male.svg'
+import CisWomanIcon from '@/assets/icons/gender/female.svg'
+import GenderfluidIcon from '@/assets/icons/gender/transgender.svg'
+import GenderqueerIcon from '@/assets/icons/gender/transgender-2.svg'
+import GenderNonconformingIcon from '@/assets/icons/gender/other-gender.svg'
+import HijraIcon from '@/assets/icons/gender/other-gender.svg'
+import IntersexIcon from '@/assets/icons/gender/intersex.svg'
+import PangenderIcon from '@/assets/icons/gender/other-gender.svg'
+import TransfeminineIcon from '@/assets/icons/gender/transgender-2.svg'
+import TransManIcon from '@/assets/icons/gender/transgender.svg'
+import TransmasculineIcon from '@/assets/icons/gender/transgender-2.svg'
+import TranssexualIcon from '@/assets/icons/gender/transgender.svg'
+import TransWomanIcon from '@/assets/icons/gender/transgender.svg'
+import TwoSpiritIcon from '@/assets/icons/gender/other-gender.svg'
+import NonBinaryIcon from '@/assets/icons/gender/non-binary-transgender.svg'
+import OtherIcon from '@/assets/icons/gender/other-gender.svg'
+
+export const genderIcons:Record<string, string> = {
+  male: Male,
+  female: Female,
+  agender: Agender,
+  androgynous: Androgynous,
+  bigender: Bigender,
+  cis_man: CisMan,
+  cis_woman: CisWoman,
+  genderfluid: Genderfluid,
+  genderqueer: Genderqueer,
+  gender_nonconforming: GenderNonconforming,
+  hijra: Hijra,
+  intersex: Intersex,
+  pangender: Pangender,
+  transfeminine: Transfeminine,
+  trans_man: TransMan,
+  transmasculine: Transmasculine,
+  transsexual: Transsexual,
+  trans_woman: TransWoman,
+  two_spirit: TwoSpirit,
+  non_binary: NonBinary,
+  other: Other,
+}
