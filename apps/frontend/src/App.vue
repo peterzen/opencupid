e5a3669ef751733a1a9f909224f3781c48ceb519
Peter Banik <peter@froggle.org>
2025-06-17 02:26:00 +0200

    Fix app viewport scrolling

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,6 +1,22 @@
 <script lang="ts" setup>
 import Navbar from '@/components/nav/Navbar.vue'
 import AppNotifier from '@/components/AppNotifier.vue'
+import { onMounted } from 'vue'
+
+// FIXME
+// This is a workaround to ensure the page scrolls down 
+// on initial load. in order to ensure on mobile devices
+// the browser address/status bar gets scrolled out of view.
+// and the page takes up the full height of the viewport.
+// onMounted(() => {
+//   setTimeout(() => {
+//     document.documentElement.scrollTop = 100
+//     document.body.scrollTop = 100
+//     // window.scrollTo(0, 100) // fallback
+//   }, 1000)
+// })
+
+
 </script>
 
 <template>
