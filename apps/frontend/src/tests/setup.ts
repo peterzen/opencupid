c10de13ed8257f75f16bedec2a0d2daea28f6eaf
Peter Banik <peter@froggle.org>
2025-06-21 10:09:24 +0200

    BootstrapVueNextResolver for vitest

--- apps/frontend/src/tests/setup.ts
+++ apps/frontend/src/tests/setup.ts
@@ -2,14 +2,5 @@ import { config } from '@vue/test-utils'
 import { BInput, BForm, BButton, BFormFloatingLabel, BFormTextarea, BFormCheckbox, BFormFile, BFormRadio, BCard, BFormInvalidFeedback } from 'bootstrap-vue-next'
 
 config.global.components = {
-  BInput,
-  BForm,
-  BButton,
-  BFormFloatingLabel,
-  BFormTextarea,
-  BFormCheckbox,
-  BFormFile,
-  BFormRadio,
-  BCard,
-  BFormInvalidFeedback,
+
 }
--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -3,14 +3,20 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 import { server, define } from './vite.common'
-
+import Components from 'unplugin-vue-components/vite'
+import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
 
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
 
   return {
     ...define(mode),
     ...server(mode),
-    plugins: [vue()],
+    plugins: [vue(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+      // exclude: [/\/__tests__\//],
+    }),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
